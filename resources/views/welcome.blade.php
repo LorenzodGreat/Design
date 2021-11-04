fa0716b46af8e59f6bbace41d7b432f7a7722911
LorenzodGreat <90816226+LorenzodGreat@users.noreply.github.com>
2021-11-03 21:01:02 -0500

    Done

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+@section('name')
+@section('main')
+    {{-- display image --}}
+    <x-uno/>
+
+    {{-- Section --}}
+    <x-dos/>
+
+      {{-- Clothing --}}
+    <x-tres/>
+    
+    {{-- Categories --}}
+    <center>
+      <img style="width: 250px;" src="https://od.lk/s/ODhfMTEwNDIwMDlf/frontlegendsoutletja.png" alt="logo">
+      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-6 text-white">Shop By Categories</h1>
+    </center>
+    <x-quatro/>
+
+@endsection
